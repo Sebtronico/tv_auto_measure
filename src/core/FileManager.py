0a629a8eb58de230ea290c0cfbb50869b51a21a6
Sebtronico <jsebastiancm96@gmail.com>
2025-02-18 02:12:45 -0500

    Eliminación de funcion

--- src/core/FileManager.py
+++ src/core/FileManager.py
@@ -4,6 +4,3 @@ class EtlFileManager:
     def __init__(self, instrument: InstrumentManager, folder_name: str):
         self.instrument = instrument
         self.folder_name = folder_name
-
-    
-    def 
