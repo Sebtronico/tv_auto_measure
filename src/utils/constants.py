67d91bb492e87d3b64a5ca577ed4352d4f25e2ad
Sebtronico <jsebastiancm96@gmail.com>
2025-02-19 00:24:33 -0500

    Adición de diccionario de bandas para FSH o FPH

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -111,8 +111,35 @@ Diccionarios para banco de mediciones
 """
 
 
+# Definición del diccionario con los parámetros de cada banda para el ETL
+BANDS_ETL = {
+    #Band      Fi      Ff      VBW   RBW  ref    tr. mode   unit 
+    '700':    [703,    803,    100,  30, -25,   'AVERage', 'DBM'],
+    '850':    [824,    894,    100,  30, -25,   'AVERage', 'DBM'],
+    '815':    [806,    824,    100,  30, -25,   'AVERage', 'DBM'],
+    '1900':   [1850,   1990,   100,  30, -25,   'AVERage', 'DBM'],
+    'AWS1':   [1755,   1780,   30,   10, -25,   'MAXHold', 'DBM'],
+    'AWS2':   [2155,   2180,   30,   10, -25,   'MAXHold', 'DBM'],
+    'AWS3':   [2170,   2200,   30,   10, -25,   'MAXHold', 'DBM'],
+    '2.5':    [2500,   2690,   100,  30, -25,   'AVERage', 'DBM'],
+    '900_1':  [894,    915,    30,   10, -60,   'MAXHold', 'DBM'],
+    '900_2':  [900,    928,    30,   10, -60,   'AVERage', 'DBM'],
+    '3500':   [3300,   3700,   100,  30, -40,   'MAXHold', 'DBM'],
+    '2.4GHz': [2400,   2483.5, 30,   10, -60,   'MAXHold', 'DBM'],
+    '5GHz':   [5180,   5825,   100,  30, -60,   'MAXHold', 'DBM'],
+    '2300MHz':[2300,   2400,   100,  30, -60,   'AVERage', 'DBM'],
+    'Enlace': [300,    330,    30,   10,  82,   'AVERage', 'DBUV'],
+    'tv_b1':  [54,     88,     30,   10,  82,   'AVERage', 'DBUVm'],
+    'tv_b3':  [174,    216,    30,   10,  82,   'AVERage', 'DBUVm'],
+    'tv_d':   [470,    512,    30,   10,  82,   'AVERage', 'DBUVm'],
+    'tv_b4':  [512,    608,    30,   10,  82,   'AVERage', 'DBUVm'],
+    'tv_b4_5':[614,    656,    30,   10,  82,   'AVERage', 'DBUVm'],
+    'tv_b5':  [656,    698,    30,   10,  82,   'AVERage', 'DBUVm'],
+    'FM':     [88,     108,    30,   10,  82,   'AVERage', 'DBUVm'],
+}
+
 # Definición del diccionario con los parámetros de cada banda
-BANDS = {
+BANDS_FXH = {
     #Band      Fi      Ff      VBW   RBW  ref    tr. mode   unit 
     '700':    [703,    803,    100,  30, -25,   'AVERage', 'DBM'],
     '850':    [824,    894,    100,  30, -25,   'AVERage', 'DBM'],
